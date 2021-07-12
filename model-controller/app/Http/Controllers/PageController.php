ae9166f27cb5b982d9b47c31a76f623e882f6cbe
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-12 14:47:37 +0200

    Collegamento al database

--- model-controller/app/Http/Controllers/PageController.php
+++ model-controller/app/Http/Controllers/PageController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
     public function index()
-    {
+    {   
+        $movies = Movie::all();
+
+        /* foreach($movies as $movie){
+                echo $movie->title;
+        } */
+        $movies->title;
+
         return view('movies.home');
     }
 }
