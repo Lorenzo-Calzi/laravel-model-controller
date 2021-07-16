8b2d5a3f8ede94986dc950c708376b0987a916bc
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-16 14:59:45 +0200

    Correzione esercizio

--- model-controller/resources/views/movies/home.blade.php
+++ model-controller/resources/views/movies/home.blade.php
@@ -1,3 +1,20 @@
 @extends ('layout.app')
 
 
+        <div class="title">
+            <i class="fas fa-film"></i>
+            <h1>FIlm ora al cinema</h1>
+            <i class="fas fa-film"></i>
+        </div>
+
+        <div class="container">
+            @foreach($movies as $movie)
+                <div class="card">
+                    <h3> {{$movie->title}} </h3>
+                    <h5> {{$movie->original_title}} </h5>
+                    <span> {{$movie->nationality}} </span>
+                    <span> {{$movie->date}} </span>
+                    <strong> {{$movie->vote}} </strong> 
+                </div>
+            @endforeach
+        </div>
